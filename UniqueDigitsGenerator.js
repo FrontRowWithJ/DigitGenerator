e0b142bc5db0498a09bc69a58dd68fdb3c964202
Michalade <adebusum@tcd.ie>
2021-01-12 14:28:27 +0000

    Update UniqueDigitsGenerator.js
    Works for exam format

--- UniqueDigitsGenerator.js
+++ UniqueDigitsGenerator.js
@@ -1,27 +1,22 @@
-const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const getUnique = (value, index, arr = []) => {
     return arr.indexOf(value) == index;
 }
 
 const generateR = () => {
-    const digits = [];
-    for (let i = 0; i < 15; i++) {
-        const digit = (Math.random() * 10) | 0;
-        digits.push(digit);
-    }
-    return digits;
+    const number = document.getElementById("student-number").value;
+    const numSquared = +number * +number;
+    return ("" + numSquared).split("").slice(0, 15).map((e) => +e);
 }
+
 const isDigitNotPresent = (value, array) => {
     return array.indexOf(value) == -1;
 }
+
 const generateDigits = () => {
     const DDigits = generateR();
     const uniques = DDigits.filter(getUnique);
-    const remainingDigits = digits.filter((value) => isDigitNotPresent(value, uniques));
+    const remainingDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter((value) => isDigitNotPresent(value, uniques));
     const UDigits = uniques.concat(remainingDigits);
-    
-    const DDigitsRow = [...document.getElementById("d-digits-row").children];
-    DDigitsRow.map((e, i) => e.innerHTML = DDigits[i]);
 
     const UDigitsRow = [...document.getElementById("u-digits-row").children];
     UDigitsRow.map((e, i) => e.innerHTML = UDigits[i]);
